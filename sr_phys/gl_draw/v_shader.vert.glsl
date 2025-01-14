e707b7e52880c4c3759124a44b2a655a51e9e4d5
Curlip <curlipcool@gmail.com>
2025-01-14 23:14:09 +1100

    I think the porblems are when t[1] ~= a.x or b.x

--- sr_phys/gl_draw/v_shader.vert.glsl
+++ sr_phys/gl_draw/v_shader.vert.glsl
@@ -10,6 +10,13 @@ layout (binding=1) uniform isampler1D wl_lens;
 
 out vec3 color;
 
+out int wl_len;
+
+out vec4 start;
+out vec4 end;
+
+out float intv_neg;
+
 uniform float sr_c;
 uniform float time;
 
@@ -25,105 +32,91 @@ float interval( vec4 a ){
     return sr_c*sr_c*t*t - x*x - y*y - z*z;
 }
 
-float[2] intersections( vec4 a, vec4 b ){
-    float[2] t = {99,99};
-
-    // Quadratic Quants
-    float intv;
-    float A;
-    float B;
-    float C;
-
-    float aa = dot(a,a);
-    float ab = dot(a,b);
-    float bb = dot(b,b);
-
-    float fact = 1.0/( (b.x-a.x)*(b.x-a.x) );
-
-    A = fact*(aa -2.0*ab + bb) - sr_c*sr_c;
-    B = -2.0*fact*(aa*a.x - ab*(a.x+b.x) + bb*a.x);
-    C = fact*(aa*b.x*b.x - 2.0*ab*a.x*b.x + bb*a.x*a.x); 
-
-    intv = B*B - 4.0*A*C;
-
-    // Logical non-starter
-    if(intv < 0) return t; // Dies silently
-
-    t[0] = ( -B - sqrt( intv ) ) / (2.0*A);
-    t[1] = ( -B + sqrt( intv ) ) / (2.0*A); // N.B t2 is larger than t1
-
-    return t;
-}
-
-vec4 line( vec4 a, vec4 b, float t ){
-    return (1.0/(b.x-a.x)) * ( a*(b.x-t) + b*(t-a.x) ); // This is the line equation.
+vec4 transform( vec4 a, vec3 scaled_offset ){
+    vec4 a_world = a + vec4(0.0, scaled_offset); // World coords.
+    return vec4(a_world.x-time, (view*vec4(a_world.yzw, 1.0)).xyz); // Camera coords
 }
 
 void main(){
-    vec4[MAX_WL_LEN] wl;
-    int wl_len = texelFetch(wl_lens, wl_index, 0).r;
+    gl_Position = projection*view*vec4(0.0,0.0,-10.0,1.0);
+    color = vec3(0.0,0.0,1.0);
 
+    vec4[MAX_WL_LEN] wl;
+    wl_len = texelFetch(wl_lens, wl_index, 0).r;
     for(uint i=0; i<wl_len; i++) wl[i] = texelFetch(wls, ivec2(i, wl_index), 0);
 
-    vec3 scaled_offset = (model*vec4(wl_offset.yzw, 1.0)).xyz;
+    vec4 scaled_offset = vec4(0.0, (model*vec4(wl_offset.yzw,1.0)).xyz);
 
-    struct {
-        vec4 a;
-        vec4 b;
-        float score;
-        float t;
-    } vertex;
+    intv_neg=0;
 
-    vec4 s;
-    float t;
+    // Infinite Extents
+    start = transform(wl[0], scaled_offset.yzw);
+    end = transform(wl[wl_len-1], scaled_offset.yzw);
+    if( start.x > end.x ){
+        vec4 tmp;
+        tmp = start;
+        start = end;
+        end = tmp;
+    }
 
-    vertex.score = 0;
+    float t_end = sqrt( dot(end, end) / (sr_c*sr_c) );
+    if(t_end > end.x){
+        gl_Position = projection*vec4(end.yzw, 1.0); 
+        color = vec3(1.0,0.0,0.0);
+        return;
+    }
 
-    for(uint i=1; i<wl_len; i++){ // Start at i=1, use i-1;
-        // TRANSFORMS
-        vec4 a_world = wl[i-1] + vec4(0.0, scaled_offset); // World coords.
-        vec4 b_world = wl[i] + vec4(0.0, scaled_offset);
+    float t_start = -sqrt( dot(start, start) / (sr_c*sr_c) );
+    if(t_start < start.x){
+        gl_Position = projection*vec4(start.yzw, 1.0); 
+        color = vec3(0.0,1.0,0.0);
+        return;
+    }
 
-        vec4 a = vec4(a_world.x-time, (view*vec4(a_world.yzw, 1.0)).xyz); // Camera coords
-        vec4 b = vec4(b_world.x-time, (view*vec4(b_world.yzw, 1.0)).xyz);
+    for(int i=1; i<wl_len; i++){
+        vec4 a = transform(wl[i-1], scaled_offset.yzw);
+        vec4 b = transform(wl[i], scaled_offset.yzw);
 
         if( a.x > b.x ){
-            // Swap, so we habe the a to b seg
             vec4 tmp;
             tmp = a;
             a = b;
             b = tmp;
         }
 
-        if(!( (interval(a) * interval(b) < 0) && (a.x<0 || b.x<0) )) return;
+        float[2] t = {1.0/0.0,-1.0/0.0};
 
-        // TODO: Apply lorentz.
-        float t[2] = intersections(a,b);
-        
-        float t_0 = 0;
+        // Quadratic Quants
+        float intv;
+        float A;
+        float B;
+        float C;
 
-        if(t[0]<0) t_0 = t[0];
-        if(t[1]<0) t_0 = t[1];
+        float aa = dot(a.yzw,a.yzw);
+        float ab = dot(a.yzw,b.yzw);
+        float bb = dot(b.yzw,b.yzw);
 
-        if(t_0>a.x && t_0<b.x && t_0!=0){
-            float score = 1.0/( abs(interval(a)) + abs(interval(b)) );
-            if( score > vertex.score ){
-                vertex.a = a;
-                vertex.b = b;
-                vertex.t = t_0;
-                vertex.score = score;
-            }
+        float fact = 1.0/( (b.x-a.x)*(b.x-a.x) );
+
+        A = fact*(aa -2.0*ab + bb) - sr_c*sr_c;
+        B = -2.0*fact*(aa*a.x - ab*(a.x+b.x) + bb*a.x);
+        C = fact*(aa*b.x*b.x - 2.0*ab*a.x*b.x + bb*a.x*a.x); 
+
+        intv =  B*B - 4.0*A*C;
+
+        // Logical non-starter
+        if(intv > 0){ 
+            t[0] = float( ( -B - sqrt( intv ) ) / (2.0*A) );
+            t[1] = float( ( -B + sqrt( intv ) ) / (2.0*A) ); // N.B t2 is larger than t1
         }
-    }
 
-    if(vertex.score != 0){
-        s = line( vertex.a, vertex.b, vertex.t );
-        t = vertex.t;
-
-        gl_Position = projection * vec4(s.yzw, 1.0);
-        color = vec3(0.0f, 0.0f, 0.0f);
-    }else{
-        gl_Position = vec4(0.0,0.0,0.0,1.0);
-        color = vec3(1.0f, 0.0f, 0.0f);
+        if( t[1]>a.x && t[1]<b.x && t[1]<0 ){
+            float t = t[1];
+            vec4 s = (1.0/(b.x-a.x)) * ( a*(b.x-t) + b*(t-a.x) ); // This is the line equation.
+            gl_Position = projection*vec4(s.yzw, 1.0); 
+            color = vec3(1.0,1.0,0.0);
+            return;
+            intv_neg+=intv;
+        }
     }
 }
