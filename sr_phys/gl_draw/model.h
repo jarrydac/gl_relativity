cec1af7bd791693af35f67bf319d989da47b48ef
Jarryd <jarryd.cairns@optusnet.com.au>
2025-01-21 00:33:02 +1100

    Seperate out mesh/model
    Will load in C with assimp. Object has one mesh + wl + model_mat,
    Model has multiple objects?

---
+++ sr_phys/gl_draw/model.h
@@ -0,0 +1,28 @@
+#ifndef SR_MESH
+#define SR_MESH
+
+#include "include/cglm/cglm.h"
+
+typedef struct {
+    vec4 position;
+} sr_mesh_vert;
+
+typedef struct {
+    unsigned int vbo_id;
+    unsigned int ebo_id;
+    unsigned int vao_id;    
+    unsigned int elements_count;
+} sr_mesh;
+
+int sr_init_mesh(
+        sr_mesh* mesh,
+        int* indicies,
+        int indicies_len,
+        sr_mesh_vert* offsets,
+        int offsets_len
+    );
+
+void sr_load_mesh(sr_mesh*, char* file);
+void sr_delete_mesh(sr_mesh*);
+
+#endif
