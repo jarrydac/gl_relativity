91898648e8f0947c25791c20faed8b40fb8d14ff
Curlip <curlipcool@gmail.com>
2025-01-21 00:33:02 +1100

    Seperate out mesh/model
    Will load in C with assimp. Object has one mesh + wl + model_mat,
    Model has multiple objects?

--- sr_phys/gl_draw/objects.h
+++ sr_phys/gl_draw/objects.h
@@ -4,25 +4,15 @@
 
 #include "include/cglm/cglm.h"
 
+#include "mesh.h"
+
 #define MAX_WL_LEN 128
 #define MAX_WL_NUM 1024
-
-typedef struct {
-    unsigned int wl_index;
-    vec4 offset;
-} sr_obj_render_vert;
 
 typedef struct {
     vec4 events[MAX_WL_LEN];
     int length;
 } sr_obj_wl;
-
-typedef struct {
-    int* indicies;
-    int indicies_len;
-    vec4* offsets;
-    int offsets_len;
-} sr_obj_mesh;
 
 typedef struct {
     unsigned int anchor_wl;
@@ -38,11 +28,11 @@ void sr_objects_init(void);     // init
 void sr_objects_render(void);   // call draw functions
 void sr_objects_close(void);    // close
 
-int sr_object_init( sr_object* object, sr_obj_wl* wl, sr_obj_mesh* mesh, mat4 model );
+int sr_object_init( sr_object* object, sr_obj_wl* wl, sr_mesh* mesh, mat4 model );
 void sr_object_delete( sr_object* );
 
 void sr_object_update_wl( sr_object* object, sr_obj_wl* wl );           // Overwrite wl in the wls texture, and length in the lengths texture
-int sr_object_update_mesh( sr_object* object, sr_obj_mesh* mesh );     // Generate new vbo and ebo
+int sr_object_update_mesh( sr_object* object, sr_mesh* mesh );     // Generate new vbo and ebo
 void sr_object_update_model( sr_object* object, mat4 model );           // Just update in datatype 
 
 void sr_object_draw( sr_object* object );                               // Draw object this frame
